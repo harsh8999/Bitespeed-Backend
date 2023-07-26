bf85cd220d83525f1b52e22817ab534e8ad52abb
harsh8999 <harsh.kumar8999@gmail.com>
2023-07-26 23:23:38 +0530

    model updated

--- src/main/java/com/bitespeed/fluxkart/models/Contact.java
+++ src/main/java/com/bitespeed/fluxkart/models/Contact.java
@@ -2,20 +2,24 @@ package com.bitespeed.fluxkart.models;
 
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Entity
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Getter
+@Setter
 public class Contact {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -25,12 +29,12 @@ public class Contact {
     int linkedId; // the ID of another Contact linked to this one
     String linkPrecedence; // "secondary"|"primary" - "primary" if it's the first Contact in the link
 
-    @CreationTimestamp
-    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     Date createdAt;
 
-    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
     Date updatedAt;
 
+    @Temporal(TemporalType.TIMESTAMP)
     Date deletedAt;
 }
